15fd1cc079a09751d20568ea8c5a836fcc9fdac2
Ben <bbgrabbag@gmail.com>
2022-01-02 06:37:57 +0000

    added readme and dev script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # docker-dev
 test docker dev environment
+
+### Getting Started
+- [Download and run initialize script](/scripts/init)
+- Run `./scripts/dev`;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.headers);
     res.writeHead(200);
-    res.end('hello world');
+    res.end('hello world!');
 });
 
 server.listen(3000, () => console.log('Listening on port 3000'));
